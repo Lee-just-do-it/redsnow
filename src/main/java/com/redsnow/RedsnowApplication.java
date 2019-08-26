ae72f398579ebaeef55ceb992ced50219abd8aed
lee <yanmo@uoko.com>
2019-08-26 16:35:59 +0800

    Initial commit

---
+++ src/main/java/com/redsnow/RedsnowApplication.java
@@ -0,0 +1,13 @@
+package com.redsnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedsnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedsnowApplication.class, args);
+    }
+
+}
