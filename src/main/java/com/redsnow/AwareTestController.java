48a5ac436b5e2715871bf0f6c68298bdb7c8391f
lee <yanmo@uoko.com>
2019-08-27 11:26:20 +0800

    增加 EnvironmentAware 使用+解释

---
+++ src/main/java/com/redsnow/AwareTestController.java
@@ -0,0 +1,30 @@
+package com.redsnow;
+
+import org.springframework.context.EnvironmentAware;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lee
+ * @desc aware bean自身能够感知xxx。。。 比如：实现 EnvironmentAware 的bean 可以感知应用上下文环境
+ * 也可以采用：
+ * @Autowired private Environment environment;
+ * 或者 @Value  @ConfigurationProperties等读取环境配置信息
+ */
+@RestController
+@SuppressWarnings("all")
+public class AwareTestController implements EnvironmentAware {
+
+    private String name;
+
+    @GetMapping("get")
+    public String get() {
+        return name;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        name = environment.getProperty("spring.application.name");
+    }
+}
